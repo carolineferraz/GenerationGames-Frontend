c1f575f3c22c0f15517a58eb89c2791eb23da4d0
Caroline Ferraz <projetos.crrz@gmail.com>
2022-08-29 16:57:57 -0300

    Feat: Implementing login() method in login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../Model/UserLogin';
+import { AuthService } from '../Service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  login() {
+    this.authService.login(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.id = this.userLogin.id
+
+      this.router.navigate(['/start'])
+    }, erro => {
+      if(erro.status == 401) {
+        alert("Incorrect User or/and Password")
+      }
+    })
   }
 
 }
