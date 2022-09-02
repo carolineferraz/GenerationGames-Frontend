b836828f420f41e59ae14a1446ed822f59fa7a66
Caroline Ferraz <projetos.crrz@gmail.com>
2022-09-01 21:35:56 -0300

    Feat: Creating logOut() method in header.component.ts and implementing it in header.component.html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { AuthService } from '../Service/auth.service';
 
 @Component({
@@ -9,12 +11,22 @@ import { AuthService } from '../Service/auth.service';
 export class HeaderComponent implements OnInit {
 
   constructor(
-    public auth: AuthService
+    public auth: AuthService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
   }
 
+  logOut() {
+    this.router.navigate(['/home'])
+    environment.token = ''
+    environment.nome = ''
+    environment.id = 0
+    environment.tipo = ''
+  }
+
+  // menu mobile scripts in progress:
   isMenuOpen = false;
 
   toggleMenu() {
